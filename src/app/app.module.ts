e560cb137d6f5210bb704841236cfb5590c309c9
filipebaieuropa <f.albuquerque@baieuropa.pt>
2021-12-15 17:15:23 +0000

    Added login service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,42 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { MenuComponent } from './components/menu/menu.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/security/login/login.component';
+import { routes } from './app.routes';
+import { UserService } from './services/user.service';
+import { SharedService } from './services/shared.service';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AuthInterceptor } from './components/security/auth.interceptor';
+import { AuthGuard } from './components/security/auth.guard';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     MenuComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    routes,
+
   ],
-  providers: [],
+  providers: [
+    UserService, 
+    SharedService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,12 @@
+import { ModuleWithProviders } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from './components/home/home.component';
+import { AuthGuard } from "./components/security/auth.guard";
+import { LoginComponent } from './components/security/login/login.component';
+
+export const ROUTES: Routes = [
+    {path: '', component: HomeComponent, canActivate: [AuthGuard]},
+    {path: 'login', component : LoginComponent }
+]
+
+export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
